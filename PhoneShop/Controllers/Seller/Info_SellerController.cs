431efa18415de4107439c9fbb699291342b1cf24
ngoclewill2002 <ngoclewill2002@gmail.com>
2024-06-27 23:09:58 +0700

    Seller_Layout_2706

---
+++ PhoneShop/Controllers/Seller/Info_SellerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneShop.Controllers.Seller
+{
+    public class Info_SellerController : Controller
+    {
+        [Route("/kenh-nguoi-ban.html")]
+        public IActionResult Info_Seller()
+        {
+            return View();
+        }
+    }
+}

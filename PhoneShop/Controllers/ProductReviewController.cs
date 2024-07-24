ea34201d61ddb0d7025cc4f7875ad06269f0543f
ngoclewill2002 <ngoclewill2002@gmail.com>
2024-07-24 14:26:51 +0700

    user_2707

---
+++ PhoneShop/Controllers/ProductReviewController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneShop.Models;
+
+namespace PhoneShop.Controllers
+{
+    public class ProductReviewController : Controller
+    {
+        private readonly ShopPhoneDbContext _context;
+
+        public ProductReviewController(ShopPhoneDbContext shopPhoneDbContext)
+        {
+
+            _context = shopPhoneDbContext;
+
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Delete_Comment(int Id)
+        {
+            if (HttpContext.Session.GetString("AccountId") == null)
+            {
+                return Json(new { success = false });
+            }
+            var taikhoanID = HttpContext.Session.GetString("AccountId")!;
+            int AccountInt = int.Parse(taikhoanID);
+
+            var checkAccount = _context.Accounts.Where(x=> x.Id == AccountInt).FirstOrDefault()!.Email;
+
+            var checkRwProduct = _context.product_Reviews.Where(x => x.Id == Id && x.UserEmail == checkAccount).FirstOrDefault();
+            if (checkRwProduct != null)
+            {
+                var GetProduct = _context.Products.Where(x=> x.Id == checkRwProduct.ProductId).FirstOrDefault()!;
+
+
+                _context.product_Reviews.Remove(checkRwProduct);
+                _context.SaveChanges();
+                //giu nguyen trang
+                return Redirect(Request.Headers["Referer"].ToString());
+
+            }
+
+
+
+            //giu nguyen trang
+            return Redirect(Request.Headers["Referer"].ToString());
+
+        }
+    }
+}
